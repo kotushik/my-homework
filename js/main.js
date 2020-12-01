81160fd58446421a1f542ddc136d878ba1eea942
kotushik <kotushik@mail.ru>
2020-12-01 18:06:23 +0300

    Добавлена анимация на сайт

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('#learn .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+        $('.section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__fadeInDown");
+	        }
+	    });
+        $('.exp-freetime').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__fadeInLeft");
+	        }
+        });
+        $('.exp-travel').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__fadeInUp");
+	        }
+        });
+        $('.exp-value').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__fadeInRight");
+	        }
+        });
+        $('.form').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("animate__rollIn");
+	        }
+	    });
+	});
+})
